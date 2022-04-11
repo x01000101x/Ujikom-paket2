ac44435743a6575e1ac2e2b7d0f5b4ace24eb149
x01000101x <leodamanik619@gmail.com>
2022-04-11 22:30:49 +0700

    checkpoint

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -14,4 +14,5 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('home');
     $router->resource('fasilitas', FasilitasController::class);
     $router->resource('kamars', RoomController::class);
+    $router->resource('users', UserController::class);
 });
